15f28cc9fe12b8b8837e13c576eac7b63cf84a73
ArthurRomansini <59968944+ArthurRomansini@users.noreply.github.com>
2020-01-19 17:44:47 -0300

    NativeJS introdução do front-end a aplicação.

---
+++ web/src/App.js
@@ -0,0 +1,23 @@
+import React, {useState} from 'react';
+import './global.css';
+import './App.css';
+// Componente: Bloco isolado
+// Propriedade: Propriedade de um componente
+// Estado: informação mantida pelo componente
+
+function App(){
+ 
+   return (
+    <div id='app'>
+      <aside>
+
+      </aside>
+      <main>
+
+
+      </main>
+    </div>
+   );
+}
+
+export default App;
